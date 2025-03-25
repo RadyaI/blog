a54e9637c30e2af282bbef25ca7c04166067639d
RadyaI <radyaiftikhar@gmail.com>
2025-03-25 09:57:25 +0700

    F animation

--- components/blog.tsx
+++ components/blog.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
+import AOS from 'aos'
+import "aos/dist/aos.css"
 
 export default function Blog() {
 
@@ -11,31 +13,37 @@ export default function Blog() {
     const search = useSearchParams()
     const searchString = search.toString()
     const [isFilter, setIsFilter] = useState<boolean>(false)
+    const [isAnimate, setIsAnimate] = useState<boolean>(false)
 
     useEffect(() => {
-        if (search.get("filter") || search.get("search")) {
+        AOS.init({ duration: 800, once: false })
+        if (search.has("filter") || search.has("search")) {
             setIsFilter(true)
         } else {
-            setIsFilter(false)
+            if (!isAnimate) setIsFilter(false)
         }
     }, [searchString, search])
 
     function clearFilter() {
-        setIsFilter(false)
+        setIsAnimate(true)
         router.replace("/")
+        setTimeout(() => {
+            setIsFilter(false)
+            setIsAnimate(false)
+        }, 2000);
     }
 
     return (
         <>
             {isFilter && (
                 <div className="mt-12 w-[90%] mx-auto p-4 mb-[200px]">
-                    <div onClick={() => clearFilter()} className="flex gap-2 items-center rounded-xl bg-red-300 cursor-pointer w-fit h-fit py-1 px-2">
+                    <div onClick={() => clearFilter()} className={`flex ${isAnimate ? "hidden" : ""} gap-2 items-center rounded-xl bg-red-300 cursor-pointer w-fit h-fit py-1 px-2`}>
                         <span className="text-xl"><IoCloseSharp /></span>
                         <p>Clear Filter</p>
                     </div>
-                    <div className="animate__animated animate__fadeInUp flex flex-wrap justify-start gap-3">
+                    <div className={`animate__animated animate__fadeInUp ${isAnimate ? "animate__fadeOutDown" : ""} flex flex-wrap justify-start gap-3`}>
 
-                        <div className="mt-4 w-full sm:w-[24%] h-[180px] relative rounded-xl cursor-pointer overflow-hidden group">
+                        <div data-aos="fade-up" className="mt-4 w-full sm:w-[24%] h-[180px] relative rounded-xl cursor-pointer overflow-hidden group">
                             <Image src={"/title/title8.jpg"} alt="Thumbnail" className="object-cover" fill />
                             <div className="absolute inset-0 bg-black/60 transition-all duration-300 group-hover:bg-black/30"></div>
                             <div className="absolute bottom-0 w-full h-1/2 p-3 text-white z-10">
@@ -43,15 +51,7 @@ export default function Blog() {
                                 <small className="opacity-80">8 Mei 2025</small>
                             </div>
                         </div>
-                        <div className="mt-4 w-full sm:w-[24%] h-[180px] relative rounded-xl cursor-pointer overflow-hidden group">
-                            <Image src={"/title/title8.jpg"} alt="Thumbnail" className="object-cover" fill />
-                            <div className="absolute inset-0 bg-black/60 transition-all duration-300 group-hover:bg-black/30"></div>
-                            <div className="absolute bottom-0 w-full h-1/2 p-3 text-white z-10">
-                                <p className="font-semibold overflow-hidden line-clamp-2 text-ellipsis h-[70%]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente molestiae rem exercitationem neque placeat nesciunt alias nam quibusdam. Ab quaerat aliquam rem provident nobis minus natus soluta maxime similique officiis.</p>
-                                <small className="opacity-80">8 Mei 2025</small>
-                            </div>
-                        </div>
-                        <div className="mt-4 w-full sm:w-[24%] h-[180px] relative rounded-xl cursor-pointer overflow-hidden group">
+                        <div data-aos="fade-up" className="mt-4 w-full sm:w-[24%] h-[180px] relative rounded-xl cursor-pointer overflow-hidden group">
                             <Image src={"/title/title8.jpg"} alt="Thumbnail" className="object-cover" fill />
                             <div className="absolute inset-0 bg-black/60 transition-all duration-300 group-hover:bg-black/30"></div>
                             <div className="absolute bottom-0 w-full h-1/2 p-3 text-white z-10">
--- components/title.tsx
+++ components/title.tsx
@@ -2,24 +2,61 @@
 
 import Image from "next/image"
 import { useRouter, useSearchParams } from "next/navigation"
+import { useEffect, useState } from "react"
 import { BiSearch } from "react-icons/bi"
 
 export default function Title() {
 
     const router = useRouter()
     const search = useSearchParams()
+    const searchString = search.toString()
+
+    const [searchValue, setSearchValue] = useState<string>("")
+    const [isFilter, setIsFilter] = useState<boolean>(false)
+    const [isAnimate, setIsAnimate] = useState<boolean>(false)
 
     function updateFilter(e: string) {
-        const params = new URLSearchParams(search.toString()); 
-        params.set("search", e);
-      
-        router.push(`?${params.toString()}`);
-      }
+        const params = new URLSearchParams(searchString)
+        params.set("search", e)
+        setSearchValue(e)
 
+        router.push(`?${params.toString()}`)
+    }
+
+    function hideImageAnimation() {
+        setIsAnimate(true)
+        setTimeout(() => {
+            setIsAnimate(false)
+            setIsFilter(true)
+        }, 500)
+    }
+
+    function showImageAnimation() {
+        setIsAnimate(true)
+        setTimeout(() => {
+            setIsAnimate(false)
+            setIsFilter(false)
+        }, 500)
+    }
+
+    useEffect(() => {
+        const hasFilter = search.has("filter")
+        const hasSearch = search.has("search")
+
+        if (hasFilter || hasSearch) {
+            if (!isFilter) hideImageAnimation()
+        } else {
+            if (isFilter) {
+                showImageAnimation()
+            } else {
+                setSearchValue("")
+            }
+        }
+    }, [searchString])
     return (
         <>
             <div className="py-2 mx-auto sm:mt-2 flex sm:h-[40dvh]">
-                <div className="-ml-10 hidden mt-5 sm:flex gap-4 sm:flex-col border-[red] w-1/3">
+                {!isFilter && (<div className={`animate__animated ${isAnimate ? "animate__fadeOutLeft" : "animate__fadeInLeft"} -ml-10 hidden mt-5 sm:flex gap-4 sm:flex-col border-[red] w-1/3`}>
                     <div className="w-full h-1/2 flex items-end justify-around">
                         <div className="w-30 h-30 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title1.png"} fill alt="Foto"></Image></div>
                         <div className="w-25 h-25 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title2.png"} fill alt="Foto"></Image></div>
@@ -31,18 +68,18 @@ export default function Title() {
                         <div className="w-15 h-15 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title6.png"} fill alt="Foto"></Image></div>
                         <div className="w-10 h-10 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title7.png"} fill alt="Foto"></Image></div>
                     </div>
-                </div>
-                <div className="p-2 w-full sm:w-1/2 flex flex-col items-center justify-center relative">
+                </div>)}
+                <div className="p-2 mx-auto w-full sm:w-1/2 flex flex-col items-center justify-center relative">
                     <div id="hiasan" className="rounded-full bg-blue-300 blur-[35px] w-15 h-15 absolute bottom-5 right-6 z-0"></div>
                     <p className="text-4xl sm:text-5xl">What&apos;s on your mind?</p>
                     <div className="z-10 flex items-center justify-center w-full mt-5">
                         <div className="cursor-pointer text-[grey] shadow-xl rounded-l-full text-3xl h-full flex items-center pl-4 pr-1 bg-white">
                             <BiSearch></BiSearch>
                         </div>
-                        <input onChange={(e) => updateFilter(e.target.value)} type="text" placeholder="Search..." className="shadow-xl bg-white rounded-r-full outline-none w-4/5 pr-5 pl-2 py-3" />
+                        <input value={searchValue} onChange={(e) => updateFilter(e.target.value)} type="text" placeholder="Search..." className="shadow-xl bg-white rounded-r-full outline-none w-4/5 pr-5 pl-2 py-3" />
                     </div>
                 </div>
-                <div className="-mr-10 hidden mt-5 sm:flex gap-4 sm:flex-col border-[red] w-1/3">
+                {!isFilter && (<div className={`animate__animated ${isAnimate ? "animate__fadeOutRight" : "animate__fadeInRight"} -mr-10 hidden mt-5 sm:flex gap-4 sm:flex-col border-[red] w-1/3`}>
                     <div className="w-full h-1/2 flex items-end justify-around">
                         <div className="w-10 h-10 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title10.png"} fill alt="Foto"></Image></div>
                         <div className="w-15 h-15 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title9.png"} fill alt="Foto"></Image></div>
@@ -54,7 +91,7 @@ export default function Title() {
                         <div className="w-25 h-25 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title13.png"} fill alt="Foto"></Image></div>
                         <div className="w-30 h-30 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title14.png"} fill alt="Foto"></Image></div>
                     </div>
-                </div>
+                </div>)}
             </div>
         </>
     )
