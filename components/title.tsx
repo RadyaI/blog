75fd8c62160ecbc01791ad662b6c6c0e08d7f766
RadyaI <radyaiftikhar@gmail.com>
2025-03-25 10:10:49 +0700

    Add scroll reveal

--- components/title.tsx
+++ components/title.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import { useRouter, useSearchParams } from "next/navigation"
 import { useEffect, useState } from "react"
 import { BiSearch } from "react-icons/bi"
+import ScrollReveal from "scrollreveal"
 
 export default function Title() {
 
@@ -40,6 +41,14 @@ export default function Title() {
     }
 
     useEffect(() => {
+        if(typeof window !== "undefined"){
+            import("scrollreveal").then((ScrollReveal) => {
+                ScrollReveal.default().reveal(".img", {reset: true, interval: 60})
+            })
+        }
+    }, [isFilter])
+
+    useEffect(() => {
         const hasFilter = search.has("filter")
         const hasSearch = search.has("search")
 
@@ -56,17 +65,17 @@ export default function Title() {
     return (
         <>
             <div className="py-2 mx-auto sm:mt-2 flex sm:h-[40dvh]">
-                {!isFilter && (<div className={`animate__animated ${isAnimate ? "animate__fadeOutLeft" : "animate__fadeInLeft"} -ml-10 hidden mt-5 sm:flex gap-4 sm:flex-col border-[red] w-1/3`}>
+                {!isFilter && (<div className={`animate__animated ${isAnimate ? "animate__fadeOutLeft" : ""} -ml-10 hidden mt-5 sm:flex gap-4 sm:flex-col border-[red] w-1/3`}>
                     <div className="w-full h-1/2 flex items-end justify-around">
-                        <div className="w-30 h-30 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title1.png"} fill alt="Foto"></Image></div>
-                        <div className="w-25 h-25 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title2.png"} fill alt="Foto"></Image></div>
-                        <div className="w-15 h-15 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title3.png"} fill alt="Foto"></Image></div>
+                        <div className="img w-30 h-30 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title1.png"} fill alt="Foto"></Image></div>
+                        <div className="img w-25 h-25 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title2.png"} fill alt="Foto"></Image></div>
+                        <div className="img w-15 h-15 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title3.png"} fill alt="Foto"></Image></div>
                     </div>
                     <div className="w-full h-1/2 flex items-start justify-around">
-                        <div className="w-30 h-30 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title4.png"} fill alt="Foto"></Image></div>
-                        <div className="w-25 h-25 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title5.png"} fill alt="Foto"></Image></div>
-                        <div className="w-15 h-15 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title6.png"} fill alt="Foto"></Image></div>
-                        <div className="w-10 h-10 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title7.png"} fill alt="Foto"></Image></div>
+                        <div className="img w-30 h-30 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title4.png"} fill alt="Foto"></Image></div>
+                        <div className="img w-25 h-25 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title5.png"} fill alt="Foto"></Image></div>
+                        <div className="img w-15 h-15 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title6.png"} fill alt="Foto"></Image></div>
+                        <div className="img w-10 h-10 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title7.png"} fill alt="Foto"></Image></div>
                     </div>
                 </div>)}
                 <div className="p-2 mx-auto w-full sm:w-1/2 flex flex-col items-center justify-center relative">
@@ -79,17 +88,17 @@ export default function Title() {
                         <input value={searchValue} onChange={(e) => updateFilter(e.target.value)} type="text" placeholder="Search..." className="shadow-xl bg-white rounded-r-full outline-none w-4/5 pr-5 pl-2 py-3" />
                     </div>
                 </div>
-                {!isFilter && (<div className={`animate__animated ${isAnimate ? "animate__fadeOutRight" : "animate__fadeInRight"} -mr-10 hidden mt-5 sm:flex gap-4 sm:flex-col border-[red] w-1/3`}>
+                {!isFilter && (<div className={`animate__animated ${isAnimate ? "animate__fadeOutRight" : ""} -mr-10 hidden mt-5 sm:flex gap-4 sm:flex-col border-[red] w-1/3`}>
                     <div className="w-full h-1/2 flex items-end justify-around">
-                        <div className="w-10 h-10 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title10.png"} fill alt="Foto"></Image></div>
-                        <div className="w-15 h-15 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title9.png"} fill alt="Foto"></Image></div>
-                        <div className="w-25 h-25 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title8.jpg"} fill alt="Foto"></Image></div>
-                        <div className="w-30 h-30 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title11.png"} fill alt="Foto"></Image></div>
+                        <div className="img w-10 h-10 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title10.png"} fill alt="Foto"></Image></div>
+                        <div className="img w-15 h-15 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title9.png"} fill alt="Foto"></Image></div>
+                        <div className="img w-25 h-25 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title8.jpg"} fill alt="Foto"></Image></div>
+                        <div className="img w-30 h-30 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title11.png"} fill alt="Foto"></Image></div>
                     </div>
                     <div className="w-full h-1/2 flex items-start justify-around">
-                        <div className="w-15 h-15 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title12.png"} fill alt="Foto"></Image></div>
-                        <div className="w-25 h-25 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title13.png"} fill alt="Foto"></Image></div>
-                        <div className="w-30 h-30 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title14.png"} fill alt="Foto"></Image></div>
+                        <div className="img w-15 h-15 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title12.png"} fill alt="Foto"></Image></div>
+                        <div className="img w-25 h-25 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title13.png"} fill alt="Foto"></Image></div>
+                        <div className="img w-30 h-30 rounded-xl relative"><Image className="object-cover rounded-xl" src={"/title/title14.png"} fill alt="Foto"></Image></div>
                     </div>
                 </div>)}
             </div>
